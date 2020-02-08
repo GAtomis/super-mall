03635751e50a5caa3ec86b7624db5ea1cc2a653c
850680822 <850680822@qq.com>
2020-02-08 21:38:43 +0800

    '导航栏初始化'

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,16 +1,30 @@
 <template>
     <div>
-        <h1>
-            Home
-        </h1>
+        
+            
+            <nar-bar>
+                <template v-slot:left>  
+                    前窗口
+                </template>
+                <template v-slot:center>
+                    中标题
+                </template>
+                <template v-slot:right>
+                    后窗口
+                </template>
+
+            </nar-bar>
+        
     </div>
 </template>
 <script>
+import narBar from 'components/common/navbar/navBar'
 export default {
-    name:'Home'
+  name: 'Home',
+  components: {
+    narBar
+  }
 }
 </script>
 <style scoped>
-
-
 </style>
